ff6f25f1a20303b55420c000ca345d7235ed0747
Eliza-2202 <elizabeth@datablitz.com.pe>
2026-04-18 23:05:54 -0500

    parte1_admin_usuario

--- mi_blog/urls.py
+++ mi_blog/urls.py
@@ -16,7 +16,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('/accounts/login/')),
+
     path('admin/', admin.site.urls),
+
+    path(
+        'accounts/login/',
+        auth_views.LoginView.as_view(template_name='registro/login.html'),
+        name='login'
+    ),
+
+    path(
+        'accounts/logout/',
+        auth_views.LogoutView.as_view(),
+        name='logout'
+    ),
 ]
